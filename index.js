9723d581a19e1032d39e8a181f59bb57534f3fa9
AdamHooper <adamhooper3@gmail.com>
2021-12-06 20:07:40 +0000

    -- complete 4c 2000 0612

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
+import  myCollection  from "./collection.js";
+import chalk from 'chalk';
 
-console.log(myCollection);
+
+//console.log(myCollection);
 
   
   function describeItem(i){
@@ -26,10 +12,16 @@ console.log(myCollection);
   let count = i.count
 
       if (count === 1){
-          console.log("I have a " + name + " Here's what I like about it: " + whatILike)
+          console.log("I have a " + chalk.cyan(name) + " Here's what I like about it: " + chalk.green(whatILike))
       } else {
-          console.log( "I have "+ count + " "+ name +"s. Here's what I like about them: " + whatILike)
+          console.log( "I have "+ chalk.yellow(count) + " "+ chalk.cyan(name) +"s. Here's what I like about them: " + chalk.green(whatILike))
       }
-  }
+    }
+  //describeItem(myCollection[1])
+  //describeItem(myCollection)
 
-  describeItem(myCollection[1])
+  function describeCollection(collection){
+    collection.forEach(describeItem);
+  }
+  
+  describeCollection(myCollection);
