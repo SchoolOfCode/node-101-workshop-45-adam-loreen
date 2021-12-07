ff06e50acf75797e973df5009246ceffae523f40
AdamHooper <adamhooper3@gmail.com>
2021-12-07 21:59:22 +0000

    --formatted

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-import  myCollection  from "./collection.js";
-import chalk from 'chalk';
-
+import myCollection from "./collection.js";
+import chalk from "chalk";
 
 //console.log(myCollection);
 
-  
-  function describeItem(i){
- 
-  let name = i.name
-  let whatILike = i.whatILike
-  let count = i.count
+function describeItem(i) {
+  let name = i.name;
+  let whatILike = i.whatILike;
+  let count = i.count;
 
-      if (count === 1){
-          console.log("I have a " + chalk.cyan(name) + " Here's what I like about it: " + chalk.green(whatILike))
-      } else {
-          console.log( "I have "+ chalk.yellow(count) + " "+ chalk.cyan(name) +"s. Here's what I like about them: " + chalk.green(whatILike))
-      }
-    }
-  //describeItem(myCollection[1])
-  //describeItem(myCollection)
-
-  function describeCollection(collection){
-    collection.forEach(describeItem);
+  if (count === 1) {
+    console.log(
+      "I have a " +
+        chalk.cyan(name) +
+        " Here's what I like about it: " +
+        chalk.green(whatILike)
+    );
+  } else {
+    console.log(
+      "I have " +
+        chalk.yellow(count) +
+        " " +
+        chalk.cyan(name) +
+        "s. Here's what I like about them: " +
+        chalk.green(whatILike)
+    );
   }
-  
-  describeCollection(myCollection);
+}
+//describeItem(myCollection[1])
+//describeItem(myCollection)
+
+function describeCollection(collection) {
+  collection.forEach(describeItem);
+}
+
+describeCollection(myCollection);
